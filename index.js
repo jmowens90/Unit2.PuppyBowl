4afa3417e64e50bc7fa542512e65ec64efe2b16a
Jaylen <jmowens90@gmail.com>
2023-11-09 13:16:30 -0500

    added the form and styled it

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const url = `https://fsa-puppy-bowl.herokuapp.com/api/2310-FSA-ET-WEB-FT/`;
+const url = `https://fsa-puppy-bowl.herokuapp.com/api/2310-FSA-ET-WEB-FT-SF/`;
 
 const main = document.querySelector(`main`);
 
@@ -22,12 +22,12 @@ const getPuppyApi = async () => {
     // Invoke the loop that applies the create section function to all of the puppy cards
     createAllPuppies();
 };
-// invoke the function to get the API info
-getPuppyApi();
+
 
 // create function to create a section that shows the puppy card and append it to the main
 const createPuppyCard = (obj) => {
     const section = document.createElement(`section`);
+    puppyCardDetails(section, obj);
     main.appendChild(section);
     const h2 = document.createElement(`h2`);
     h2.innerText = obj.name;
@@ -42,4 +42,27 @@ const createAllPuppies = () => {
     state.puppyRoster.map((value) => createPuppyCard(value));
 };
 
+// add an event listener so that when the puppy card is clicked it creates a new html(without leaving the page) and populates all the info for that specific puppy
+const puppyCardDetails = (link, data) => {
+    link.addEventListener(`click`, () => {
+        html=`
+        <section>
+        <h2>${data.name}</h2>
+        <h3>${data.breed}</h3>
+        <img src="${data.imageUrl}"></img>
+        <h3>${data.status}</h3>
+        <button onClick="render()" id="goBack"> Go Back </button>
+        </section>`
+        main.innerHTML = html;
+    });
+};
+// render function that resets the html and then reapplies the createAllPuppies function for when the addevent listener click occurs
+
+const render = () => {
+    main.innerHTML = ``;
+    createAllPuppies();
+}
+
+// invoke the function to get the API info
+getPuppyApi();
 console.log(state.puppyRoster)
