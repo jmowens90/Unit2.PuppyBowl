ea4dbf0b6cd4548410b5f854b088ca937fa2cb63
Jaylen <jmowens90@gmail.com>
2023-11-09 16:41:06 -0500

    created the addEventListener for the form and created the function to add the form data to the API

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const url = `https://fsa-puppy-bowl.herokuapp.com/api/2310-FSA-ET-WEB-FT-SF/`;
 
 const main = document.querySelector(`main`);
+const form = document.querySelector(`form`);
+const puppyForm = {};
 
 // create object that holds the API information
 const state = {
@@ -12,7 +14,6 @@ const getPuppyApi = async () => {
     try {
         const response = await fetch(`${url}players`);
         const puppyInfo = await response.json();
-        console.log(puppyInfo);
         //Was getting errors becaue I only had puppyInfo. data so state.puppyRoster was returning as a blank array. Had to revisit the API data to realize the actual info was nested further inside players.
         state.puppyRoster = puppyInfo.data.players;
         console.log(state.puppyRoster);
@@ -23,23 +24,24 @@ const getPuppyApi = async () => {
     createAllPuppies();
 };
 
+// create function to apply the createPuppyCard function to all of the objects in the state object array that was created earlier
+const createAllPuppies = () => {
+    state.puppyRoster.map((value) => createPuppyCard(value));
+};
 
 // create function to create a section that shows the puppy card and append it to the main
 const createPuppyCard = (obj) => {
     const section = document.createElement(`section`);
-    puppyCardDetails(section, obj);
-    main.appendChild(section);
     const h2 = document.createElement(`h2`);
     h2.innerText = obj.name;
     section.appendChild(h2);
     const puppyImg = document.createElement(`img`);
     puppyImg.src = obj.imageUrl;
     section.appendChild(puppyImg);
-};
+    main.appendChild(section);
 
-// create function to apply the createPuppyCard function to all of the objects in the state object array that was created earlier
-const createAllPuppies = () => {
-    state.puppyRoster.map((value) => createPuppyCard(value));
+    //added the function to run the event listener on click
+    puppyCardDetails(section, obj);
 };
 
 // add an event listener so that when the puppy card is clicked it creates a new html(without leaving the page) and populates all the info for that specific puppy
@@ -56,6 +58,43 @@ const puppyCardDetails = (link, data) => {
         main.innerHTML = html;
     });
 };
+
+// create the form that can add the new puppy to the API
+
+form.addEventListener(`submit`, (event) => {
+    event.preventDefault();
+    const puppyName = document.getElementById(`puppyNameInput`).value;
+    const puppyBreed = document.getElementById(`puppyBreedInput`).value;
+    const puppyImgUrl = document.getElementById(`puppyImageUrlInput`).value;
+    const puppyStatus = document.getElementById(`puppyStatusInput`).value;
+
+    puppyForm.name = puppyName;
+    puppyForm.breed = puppyBreed;
+    puppyForm.imageUrl = puppyImgUrl;
+    puppyForm.status = puppyStatus.toLowerCase();
+
+    addNewPuppy(puppyForm);
+    state.puppyRoster.push(puppyForm);
+
+    render();
+    form.reset();
+})
+
+// function to add a new puppy to the API
+const addNewPuppy = async (puppy) => {
+    try {
+        const response = await fetch(`${url}players`, {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json',},
+            body: JSON.stringify(puppy),
+        })
+        console.log(response);
+    } catch {
+        console.log(`Error adding new recipe`)
+    }
+};
+
+
 // render function that resets the html and then reapplies the createAllPuppies function for when the addevent listener click occurs
 
 const render = () => {
@@ -65,4 +104,4 @@ const render = () => {
 
 // invoke the function to get the API info
 getPuppyApi();
-console.log(state.puppyRoster)
+
